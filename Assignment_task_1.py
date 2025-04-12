6806349ba0e9160d474cfa50203baa4ea9f62831
Sunny Mhatre <151615149+MhatreSunny@users.noreply.github.com>
2025-04-12 12:05:08 +0530

    Add files via upload

---
+++ Assignment_task_1.py
@@ -0,0 +1,13 @@
+### Task 1: Perform Basic Mathematical Operations
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+addition = num1 + num2
+sub = num1 - num2
+division = num1 / num2
+multiply = num1 * num2
+
+print(f"Addition : {addition}")
+print(f"Substraction : {sub}")
+print(f"Division : {division}")
+print(f"Multiplication : {multiply}")
---
+++ Assignment_task_2.py
@@ -0,0 +1,7 @@
+### Task 2: Create a Personalized Greeting
+
+str1 = input("Enter your First name : ")
+str2 = input("Enter your second name : ")
+
+add = str1 + " " + str2
+print(f"Hello {add}! Welcome to Python Program.")
