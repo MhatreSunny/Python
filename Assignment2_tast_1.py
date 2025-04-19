968ea364277c8e5074971e93fb62f1a9e69cd28d
Sunny Mhatre <151615149+MhatreSunny@users.noreply.github.com>
2025-04-19 16:25:02 +0530

    Add files via upload

---
+++ Assignment2_tast_1.py
@@ -0,0 +1,10 @@
+# Task1 : Check the number is Even or Odd
+
+x= int(input("Enter a number : "))
+# print(x)
+
+if x%2:
+    print(f"{x} is an even number.")
+else:
+    print(f"{x} is an odd number.")
+
