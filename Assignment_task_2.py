313b71370b942dfa8789d04ac572fd61b28e9cc4
Sunny Mhatre <151615149+MhatreSunny@users.noreply.github.com>
2025-04-21 23:22:29 +0530

    Update Assignment1_task_2.py

--- Assignment_task_2.py
+++ Assignment_task_2.py
@@ -1,4 +1,10 @@
-### Task 2: Create a Personalized Greeting
+'''
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
 
 str1 = input("Enter your First name : ")
 str2 = input("Enter your second name : ")
