76b2e23c348c914326954915851cfd167cc191f4
Sunny Mhatre <151615149+MhatreSunny@users.noreply.github.com>
2025-04-21 23:24:23 +0530

    Update Assignment2_tast_1.py

--- Assignment2_tast_1.py
+++ Assignment2_tast_1.py
@@ -1,4 +1,10 @@
-# Task1 : Check the number is Even or Odd
+'''
+Task 1: Check if a Number is Even or Odd
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
+'''
 
 x= int(input("Enter a number : "))
 # print(x)
